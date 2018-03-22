b597d040dc7f8c3d12431012fe79a21dca832b5f
Josh Kernick <joshkernick@gmail.com>
2018-03-22 22:26:46 +0000

    changes

---
+++ header.js
@@ -0,0 +1,16 @@
+document.write('\
+\
+<header id="header">\
+	<div class="logo"><a href="index.html">Derigo Consulting</a></div>\
+	<a href="#menu"><span>Menu</span></a>\
+</header>\
+\
+<nav id="menu">\
+	<ul class="links">\
+		<li><a href="index.html">Home</a></li>\
+		<li><a href="generic.html">Articles</a></li>\
+		<li><a href="elements.html">About Derigo</a></li>\
+	</ul>\
+</nav>\
+\
+');
